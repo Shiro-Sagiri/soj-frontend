e52616f1e9e64de5c6dccedd3f01569a99f6fafb
Shiro <1796475983@qq.com>
2023-09-02 11:49:26 +0800

    项目初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="global-header">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item key="0" :style="{ padding: 0, marginRight: '38px' }" disabled>
+        <div class="title-bar">
+          <img class="logo" src="../assets/shiro2.png" alt="" />
+          <div class="title">Shiro OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 40px;
+}
+.title-bar {
+  display: flex;
+  align-items: center;
+  height: 64px;
+  padding: 0 24px;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+}
+.title {
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 16px;
+}
+</style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<template>
+  <a-layout style="height: 400px">
+    <a-layout-header class="header">
+      <global-header></global-header>
+    </a-layout-header>
+    <a-layout-content class="content">
+      <router-view></router-view>
+    </a-layout-content>
+    <a-layout-footer class="footer">
+      <a href="https://github.com/Shiro-Sagiri" target="_blank"> soj by Shiro </a>
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+.header {
+  background-color: red;
+}
+
+.content {
+  background: linear-gradient(to right, #eee, #fff);
+}
+
+.footer {
+  background-color: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.footer a {
+  color: #7eb953ff;
+  text-decoration: none;
+}
+</style>
