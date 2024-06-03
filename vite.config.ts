769d34c46d149abff9575c79f624b8663324f016
Shiro <108655694+Shiro-Sagiri@users.noreply.github.com>
2024-06-03 15:02:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/soj-frontend/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
