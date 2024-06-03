cff2b450e05239243dce4e572916b1c22f0fa8f4
Shiro <108655694+Shiro-Sagiri@users.noreply.github.com>
2024-06-03 22:45:08 +0800

    Update OpenAPI.ts

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://shiro.work:8001/api',
+    BASE: 'https://shiro.work/api',
     VERSION: '0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
